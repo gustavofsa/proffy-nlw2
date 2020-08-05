b98f17342726b3321b9c7f2ffe171b87408c0635
Gustavo Farias <gugafarias2009@gmail.com>
2020-08-04 21:18:47 -0300

    :construction: initial config on backend

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express, { json } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send("Hello World!");
+});
+
+app.listen(3333);
