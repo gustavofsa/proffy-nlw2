8190e8f32c03b67870e83111c50ba290131e74f6
Gustavo Farias <gugafarias2009@gmail.com>
2020-08-05 20:15:57 -0300

    :card_file_box: Config database and create tables with knex and sqlite

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.send("Hello World!");
+  return res.send({ message: "Hello World" });
 });
 
 app.listen(3333);
