73efe73195a23b817287ccef0cf320fac9f18c6f
Gustavo Farias <gugafarias2009@gmail.com>
2020-08-07 20:10:37 -0300

    :sparkles: Search feat on teacher list

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,34 +4,43 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return(
       <article className="teacher-item">
         <header>
-          <img src="https://avatars3.githubusercontent.com/u/59898313?s=460&u=9de75bb6879098b9ee57bc050b445b552d6504e6&v=4" alt="Gustavo Farias"/>
+          <img src={teacher.avatar} alt={teacher.name}/>
           <div>
-            <strong>Gustavo Farias</strong>
-            <span>Inglês</span>
+            <strong>{teacher.name}</strong>
+            <span>{teacher.subject}</span>
           </div>
         </header>
+
+        <p>{teacher.bio}</p>
+
+        <footer>
           <p>
-            Durante a infância aprendeu a lingua inglesa durante seus sonhos, falando de maneira nativa.
-
-            <br/> <br/>
-
-            Agora com mais experiência e tendo visitado 1236287481 países, ensina inglês de maneira maestral, pois se conseguiu sonhando por que não consegues acordado !?
+            Preço/hora
+            <strong>R$ {teacher.cost}</strong>
           </p>
-
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 299,90</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="Whatsapp"/>
-              Entrar em contato
-            </button>
-          </footer>
+          <a href={`https://wa.me/${teacher.whatsapp}`} >
+            <img src={whatsappIcon} alt="Whatsapp"/>
+            Entrar em contato
+          </a>
+        </footer>
       </article>
   );
 }
