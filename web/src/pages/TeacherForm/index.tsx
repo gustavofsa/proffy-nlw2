5cab758edad5c06b44c2e71dd2e019f78c6a8a87
Gustavo Farias <gugafarias2009@gmail.com>
2020-08-07 18:29:10 -0300

    :sparkles: Navigation to landing when register class successfully

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState, FormEvent } from "react";
 
+import { useHistory } from "react-router-dom";
+
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
 import Textarea from "../../components/Textarea";
@@ -12,6 +14,8 @@ import api from "../../services/api";
 import "./styles.css";
 
 function TeacherForm() {
+  const history = useHistory();
+
   const [name, setName] = useState("");
   const [avatar, setAvatar] = useState("");
   const [whatsapp, setWhatsapp] = useState("");
@@ -59,6 +63,7 @@ function TeacherForm() {
       })
       .then(() => {
         alert("Cadastro realizado com sucesso!");
+        history.push("/");
       })
       .catch(() => {
         alert("Erro no cadastro.");
